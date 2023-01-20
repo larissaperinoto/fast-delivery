3c0da549a138c7fd0f42e29065e29549de6dd98d
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-20 19:00:53 -0300

    Insere migrations para criar o banco de dados

---
+++ back-end/src/database/migrations/sales.migration.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('sales', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        reference: {
+          model: 'users',
+          key: 'id'
+        },
+      },
+      seller_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        reference: {
+          model: 'users',
+          key: 'id'
+        },
+      },
+      total_price: {
+        type: Sequelize.DECIMAL
+      },
+      delivery_address: {
+        type: Sequelize.STRING
+      },
+      delivery_number: {
+        type: Sequelize.STRING
+      },
+      sale_date: {
+        type: Sequelize.DATE
+      },
+      status: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('sales');
+  }
+};
