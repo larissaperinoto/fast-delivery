26d461b076e414fe5dc8890737028a8eaabf5f38
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-23 19:30:11 -0300

    Insere rota GET de produtos

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const productsRoutes = require('../routes/products.routes');
 
 const app = express();
 
+app.use('/products', productsRoutes);
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
