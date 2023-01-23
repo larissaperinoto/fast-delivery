da8b6273db7f855b3d3ee39961da334833f5f32a
rafaeldev13 <rafael.13.alencar13@gmail.com>
2023-01-23 18:07:35 -0500

    Groupo-main-16 Requisito 03

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Login() {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  const checkPassword = () => {
+    const minPasswordCharacters = 6;
+    return password.length >= minPasswordCharacters;
+  };
+  const checkEmail = () => {
+    const validEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    return email.match(validEmail);
+  };
+
+  useEffect(() => {
+    if (checkEmail() && checkPassword()) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [email, password]);
+
   return (
     <form>
       <label htmlFor="common_login__input-email">
         <input
+          value={ email }
+          onChange={ ({ target }) => setEmail(target.value) }
           type="email"
           placeholder="Email"
           data-testid="common_login__input-email"
@@ -12,12 +35,18 @@ export default function Login() {
       </label>
       <label htmlFor="common_login__input-password">
         <input
+          value={ password }
+          onChange={ ({ target }) => setPassword(target.value) }
           type="password"
           placeholder="Password"
           data-testid="common_login__input-password"
         />
       </label>
-      <button type="submit" data-testid="common_login__button-login">
+      <button
+        type="submit"
+        data-testid="common_login__button-login"
+        disabled={ disabled }
+      >
         Login
       </button>
       <button type="button" data-testid="common_login__button-register">
