fcd65e9424ed1afe72203e8e6d4390d4f692fa59
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-23 19:57:22 -0300

    Insere testes da barra de navegação na página de produtos

---
+++ front-end/src/tests/products.test.js
@@ -0,0 +1,18 @@
+import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Products from '../pages/Products';
+
+describe('Testa o componente Login', () => {
+  test('Verifica se os elementos da barra nav estão presentes na tela', () => {
+    render(<BrowserRouter><Products /></BrowserRouter>);
+    expect(screen.getByTestId('customer_products__element-navbar-link-products'))
+      .toBeInTheDocument();
+    expect(screen.getByTestId('customer_products__element-navbar-link-orders'))
+      .toBeInTheDocument();
+    expect(screen.getByTestId('customer_products__element-navbar-user-full-name'))
+      .toBeInTheDocument();
+    expect(screen.getByTestId('customer_products__element-navbar-link-logout'))
+      .toBeInTheDocument();
+  });
+});
