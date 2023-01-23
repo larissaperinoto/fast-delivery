1846c3f4af3f04b2869518ff7e69caa0c5779bb1
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-23 18:49:14 -0300

    Insere rota de produtos no fluxo de cliente

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
+import Products from './pages/Products';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Navigate to="/login" /> } />
+      <Route path="/customer/products" element={ <Products /> } />
       <Route path="/login" element={ <Login /> } />
+      <Route path="/" element={ <Navigate to="/login" /> } />
     </Routes>
   );
 }
