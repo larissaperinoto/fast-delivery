490ebf557f1bc6ce0fa0f5a8fdf291eb7a7eb605
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-24 16:35:31 -0300

    Insere valor default para segredo JWT

--- back-end/src/api/utils/jwt.js
+++ back-end/src/api/utils/jwt.js
@@ -3,11 +3,12 @@ require('dotenv/config');
 const jwt = require('jsonwebtoken');
 
 const createToken = (data) => {
-  const token = jwt.sign({ data }, process.env.JWT_SECRET, {
+  const token = jwt.sign({ data }, process.env.JWT_SECRET || 'mySecret', {
     expiresIn: '1d',
     algorithm: 'HS256',
   });
 
+  console.log(token);
   return token;
 };
 
