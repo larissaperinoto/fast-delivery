d31d207f37d147d4a44b00df523faa121ad3d2f4
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-24 17:26:00 -0300

    Faz correções de lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const productsRoutes = require('../routes/products.routes');
 const route = require('./routes');
-const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: 'http://localhost:3000',
   credentials: true,
 };
 
