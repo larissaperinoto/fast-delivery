c70e3a7cb7e6bf3b18e29e7a0c9ac519d8df4487
jcaluiz <lacjunior129.la@gmail.com>
2023-01-24 02:27:02 -0300

    requisito 8 concluído

--- front-end/src/pages/registro.js
+++ front-end/src/pages/registro.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  const checkFormat = () => {
+    const validName = name.length > 11;
+    const validEmail = (/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/).test(email);
+    const validPassword = password.length > 5;
+    if (validName && validEmail && validPassword) return setDisabled(false);
+    setDisabled(true)
+  };
+
+  useEffect(() => {
+    checkFormat()
+  }, [name, email, password])
+
   return (
     <form>
       <label htmlFor="nome-de-registro">
@@ -9,6 +26,8 @@ export default function Register() {
           type="name"
           placeholder="Seu nome"
           data-testid="common_register__input-name"
+          onChange={ ({target}) => setName(target.value) }
+          value={ name }
         />
       </label>
       Email
@@ -17,6 +36,8 @@ export default function Register() {
           type="email"
           placeholder="seuemail@exemplo.com"
           data-testid="common_register__input-email"
+          onChange={ ({target}) => setEmail(target.value) }
+          value={ email }
         />
       </label>
       <label htmlFor="registro-password">
@@ -25,11 +46,14 @@ export default function Register() {
           type="password"
           placeholder="********"
           data-testid="common_register__input-password"
+          onChange={ ({target}) => setPassword(target.value) }
+          value={ password }
         />
       </label>
       <button
         type="submit"
         data-testid="common_register__button-register"
+        disabled={ disabled }
       >
         Cadastrar
       </button>
