b5a00f0ae9ba3e4df49badfae6c1b2e517df9961
rafaeldev13 <rafael.13.alencar13@gmail.com>
2023-01-25 13:33:22 -0500

    Apaga comentários no products.js

--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -35,31 +35,3 @@ function Products() {
 }
 
 export default Products;
-// import React, { useEffect, useState } from 'react';
-// import { requestProducts } from '../services/requests';
-
-// export default function Products() {
-//   const [products, setProducts] = useState([]);
-
-//   useEffect(() => {
-//     const productsRequest = async () => {
-//       const productsList = await requestProducts();
-//       console.log('oi', productsList);
-//       setProducts(productsList);
-//     };
-//     productsRequest();
-//   }, []);
-
-//   console.log(products);
-
-//   return (
-//     <div>
-//       <nav>
-//         <div data-testid="customer_products__element-navbar-link-products" />
-//         <div data-testid="customer_products__element-navbar-link-orders" />
-//         <div data-testid="customer_products__element-navbar-user-full-name" />
-//         <div data-testid="customer_products__element-navbar-link-logout" />
-//       </nav>
-//     </div>
-//   );
-// }
