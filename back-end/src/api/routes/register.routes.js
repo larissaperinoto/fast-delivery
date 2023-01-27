8551d5e76b73123ba070ed247b4915d7990bfa79
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-27 17:44:32 -0300

    Faz correções do middleware da rota register

--- back-end/src/api/routes/register.routes.js
+++ back-end/src/api/routes/register.routes.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
 
 const registerController = require('../controllers/register.controller');
-const validateRegister = require('../midlewares/validateRegister.midleware');
+// const validateRegister = require('../midlewares/validateRegister.midleware');
 
 const route = Router();
 
-route.post('/register', validateRegister, registerController.register);
+route.post('/register', registerController.register);
 
 module.exports = route;
