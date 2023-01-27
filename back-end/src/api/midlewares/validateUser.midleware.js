90653a88b8705bbba82b2c89a943d73e605f9f46
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-26 21:51:23 -0300

    Corrige middleware da rota de login.

--- back-end/src/api/midlewares/validateUser.midleware.js
+++ back-end/src/api/midlewares/validateUser.midleware.js
@@ -1,30 +1,9 @@
-const StatusCode = require('../shared/statusCode');
-
-const regExp = /^\w+@\D+\.\D+$/;
-const regExp2 = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+const validateLoginFields = require('../utils/validateLoginFields');
 
 const validateLogin = (req, res, next) => {
-  const { email, password } = req.body;
+  const message = validateLoginFields(req.body);
+  if (message) return res.status(400).json({ message });
 
-  if (!email) {
-    return res.status(StatusCode.BadRequest).json({ message: 'email is required' });
-  }
-
-  if (!password) {
-    return res.status(StatusCode.BadRequest).json({ message: 'password is required' });
-  }
-
-  const validateEmail = regExp.test(email) || regExp2.test(email);
-
-  if (!validateEmail) {
-    return res.status(StatusCode.BadRequest)
-      .json({ message: 'O email deve ter o formato "email@email.com"' });
-  }
-
-  if (password.length < 6) {
-    return res.status(StatusCode.BadRequest)
-      .json({ message: 'password is need six characteres' });
-  }
   next();
 };
 
