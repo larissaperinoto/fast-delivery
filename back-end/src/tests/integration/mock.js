0456e14e8edcb8d60af25afcf99273600a4b54ec
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-29 17:00:45 -0300

    Insere arquivos de mock

---
+++ back-end/src/tests/integration/mock.js
@@ -0,0 +1,11 @@
+const tokenMock = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjozLCJuYW1lIjoiQ2xpZW50ZSBaw6kgQmlyaXRhIiwiZW1haWwiOiJ6ZWJpcml0YUBlbWFpbC5jb20iLCJyb2xlIjoiY3VzdG9tZXIifSwiaWF0IjoxNjc1MDA2ODI3LCJleHAiOjE2NzUwOTMyMjd9.REKqYzObQyfMtXoW8NnNrCp7k4cl2XgSFq3vbIS_eg8';
+
+const userMock = {
+  email: 'cliente@email.com',
+  id: '1',
+  name: 'Cliente da Silva',
+  role: 'customer',
+};
+
+
+module.exports = { tokenMock, userMock };
