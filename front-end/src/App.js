23938d64818e6af791db25d07671cf27d80ca5bd
Larissa Perinoto <perinotolarissa@gmail.com>
2023-02-07 15:18:16 -0300

    Refatora nome dos arquivos da pasta pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Products from './pages/Products';
-import Register from './pages/registro';
-import Checkout from './pages/checkout';
-import SellerOrder from './pages/sellerOrder';
-import AdminManage from './pages/AdminManage';
-import CustomerOrders from './pages/CustomerOrders';
-import CustomerOrderDetails from './pages/CustomerOrderDetails';
-import SellerOrderDetails from './pages/SellerOrderDetails';
+import {
+  Login,
+  Register,
+  AdminManage,
+  CustomerOrderDetails,
+  CustomerOrders,
+  Products,
+  Checkout,
+  SellerOrderDetails,
+  SellerOrders,
+} from './pages';
 
 function App() {
   return (
     <Routes>
-      <Route path="/admin/manage" element={ <AdminManage /> } />
-      <Route path="/customer/orders" element={ <CustomerOrders /> } />
-      <Route path="/customer/products" element={ <Products /> } />
-      <Route path="/login" element={ <Login /> } />
-      <Route path="/register" element={ <Register /> } />
-      <Route path="/" element={ <Navigate to="/login" /> } />
-      <Route path="/customer/checkout" element={ <Checkout /> } />
-      <Route path="/seller/orders" element={ <SellerOrder /> } />
       <Route path="/customer/orders/:id" element={ <CustomerOrderDetails /> } />
       <Route path="/seller/orders/:id" element={ <SellerOrderDetails /> } />
+      <Route path="/admin/manage" element={ <AdminManage /> } />
+      <Route path="/seller/orders" element={ <SellerOrders /> } />
+      <Route path="/customer/checkout" element={ <Checkout /> } />
+      <Route path="/customer/products" element={ <Products /> } />
+      <Route path="/customer/orders" element={ <CustomerOrders /> } />
+      <Route path="/register" element={ <Register /> } />
+      <Route path="/login" element={ <Login /> } />
+      <Route path="/" element={ <Navigate to="/login" /> } />
     </Routes>
   );
 }
---
+++ front-end/src/pages/index.js
@@ -0,0 +1,21 @@
+import Login from './Login';
+import Register from './Register';
+import Products from './Products';
+import Checkout from './Checkout';
+import SellerOrders from './SellerOrders';
+import SellerOrderDetails from './SellerOrderDetails';
+import CustomerOrderDetails from './CustomerOrderDetails';
+import CustomerOrders from './CustomerOrders';
+import AdminManage from './AdminManage';
+
+export {
+  Login,
+  Register,
+  Products,
+  Checkout,
+  SellerOrders,
+  SellerOrderDetails,
+  CustomerOrderDetails,
+  CustomerOrders,
+  AdminManage,
+};
